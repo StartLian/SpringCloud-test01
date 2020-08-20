2de9435d15a6a392415cfd75194fffbb9f7533ac
lianjipeng <lianjipeng@wanji.net.cn>
2020-08-20 11:18:33 +0800

    完成Zuul单体，待测试

---
+++ Zuul/src/main/java/com/Jessica/springcloud/zuul/Zuul/fillter/PreRequestFilter.java
@@ -0,0 +1,41 @@
+package com.Jessica.springcloud.zuul.Zuul.fillter;
+
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+//在过滤器中存储客户端发起请求的时间戳
+//
+@Component
+public class PreRequestFilter extends ZuulFilter {
+
+  @Override
+  public String filterType() {
+      return FilterConstants.PRE_TYPE;
+  }
+
+  @Override
+  public int filterOrder() {
+      return 0;
+  }
+
+  @Override
+  public boolean shouldFilter() {
+      //是否启用当前的过滤器
+      return true;
+  }
+
+  @Override
+  public Object run() throws ZuulException {
+
+      //用于在过滤器之间传递消息
+      RequestContext ctx=RequestContext.getCurrentContext();
+      ctx.set("startTime",System.currentTimeMillis());
+
+
+      return null;
+  }
+}
